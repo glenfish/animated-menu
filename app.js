79208c9bd87aad7f2388de014eec0c1bdb826b61
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-03-27 19:14:33 +1100

    added second level to menu

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
+$submenu = false;
+$submenuOpened = false;
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const aboutLinks = document.querySelector(".about-links");
+const aboutLinksLi = document.querySelector(".about-links li");
 const links = document.querySelectorAll(".nav-links li");
-
+const about = document.querySelector('.nav-links li.about');
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+    if ($submenu === false) { 
+        if ($submenuOpened === true) {
+            aboutLinks.classList.toggle("open");
+            // navLinks.classList.toggle("open");
+            $submenuOpened = false;
+        }
+        navLinks.classList.toggle("open");
+        links.forEach(link => {
+            link.classList.toggle("fade");
+        });
+    } else {
+        
+        navLinks.classList.toggle("open");
+        aboutLinks.classList.toggle("open");
+        $submenuOpened = false;
+        $submenu = false;
+    }
     
 
 });
-navLinks.addEventListener("click", () => {
+
+about.addEventListener("click", () => {
+    // alert("hey!");
+    ($submenu === false ) ? $submenu = true : $submenu = false;
+    $submenuOpened = true;
     navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+    aboutLinks.classList.toggle("open");
 })
